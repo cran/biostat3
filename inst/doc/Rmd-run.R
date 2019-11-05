0f534a642bc9e2c27d289bd4db269dde8b04a193
Mark Clements <mark.clements@ki.se>
2019-11-05 20:50:02 +0000

    version 0.1.4

--- inst/doc/Rmd-run.R
+++ inst/doc/Rmd-run.R
@@ -1,20 +1,21 @@
 library(rmarkdown)
+library(prettydoc)
 setwd("~/src/R/biostat3/inst/doc")
 
 files <- paste0("q", c(1:4,6:13,22,23,25,28),".Rmd")
-files <- paste0("q", c(8),".Rmd")
+## files <- paste0("q", c(8),".Rmd")
 
-rmarkdown::render("index.Rmd", "html_document")
+rmarkdown::render("index.Rmd", html_pretty())
 
 setwd("labs")
 for (file in files) {
-    rmarkdown::render(file, "html_document")
+    rmarkdown::render(file, html_pretty())
 }
 setwd("..")
 
 setwd("solutions")
 for (file in files) {
-    rmarkdown::render(file, "html_document")
+    rmarkdown::render(file, html_pretty())
 }
 setwd("..")
 
